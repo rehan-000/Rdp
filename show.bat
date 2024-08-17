f321181e6087823c8d18c0771b211f552910f395
rehan-000 <151393986+rehan-000@users.noreply.github.com>
2024-08-17 01:19:11 -0700

    Add files via upload

---
+++ show.bat
@@ -0,0 +1,17 @@
+@echo off
+start "" /MAX "C:\Users\Public\Desktop\VMQuickConfig"
+python -c "import pyautogui as pag; pag.click(143, 487, duration=5)"
+python -c "import pyautogui as pag; pag.click(155, 554, duration=2)"
+python -c "import pyautogui as pag; pag.click(637, 417, duration=2)"
+python -c "import pyautogui as pag; pag.click(588, 10, duration=2)"
+echo ..........................................................
+echo -R "${RED}RRRRR   RRRR    "
+echo -E "${RED}R    R  R   R   "
+echo -H "${RED}RRRRR   RRRR    "
+echo -A "${RED}R   R   R  R    "
+echo -N "${RED}R    R  R   R   "
+echo -R "${RED}R     R R    R  "
+echo ..........................................................
+echo ..........................................................
+echo User name : runneradmin
+echo User Pass : TheDisa1a
